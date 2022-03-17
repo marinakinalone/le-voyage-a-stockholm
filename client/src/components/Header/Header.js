619e9398121374c0916de13f4241ffb8fd2bf737
Salt Developer <salt-dev@Dev012.local>
2022-03-17 13:39:02 +0100

    organize components folder structure and write tests

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+      <header className="header">
+        <h1 className="header__title">Le Voyage à Stockholm</h1>
+      </header>
+    )
+  }
+  
+export default Header;
+  
