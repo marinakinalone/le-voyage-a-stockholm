d492361351dd3d2bd91adba270bae6cb2ceed6ff
Salt Developer <salt-dev@Dev012.local>
2022-03-17 10:31:55 +0100

    write json files

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 const port = 3000;
-console.log('test');
+
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
 });
