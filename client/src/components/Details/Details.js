393bdb344e79982794d1e4497868afb688b03803
Salt Developer <salt-dev@Dev012.local>
2022-03-17 22:26:53 +0100

    add details box

--- client/src/components/Details/Details.js
+++ client/src/components/Details/Details.js
@@ -1,8 +1,11 @@
 import React from 'react'
 
-const Details = () => {
+const Details = ({typesInfo}) => {
   return (
-    <div>Details</div>
+    <sections className="details">
+        <p className="details_description">{typesInfo[4].description}</p>
+        <p className="details__links">créé par <a href="https://kinalone.dev">mks</a> | <a href="https://github.com/marinakinalone">GitHub</a></p>
+    </sections>
   )
 }
 
