c181f7ce462876c7af61db3905c61d0c07916e0b
Salt Developer <salt-dev@Dev012.local>
2022-03-17 21:13:42 +0100

    modify database

--- client/src/components/Markers/Markers.js
+++ client/src/components/Markers/Markers.js
@@ -1,13 +1,16 @@
 import { Marker} from '@react-google-maps/api';
 import { markerIcon } from './helpers';
 
+
+
 const Markers = ({ locations, types }) => {
     return (
         <>
         {locations.map(location => {
             return <Marker
+            key={location.place_id}
             position={location.position}
-            icon={markerIcon("blue")} 
+            icon={markerIcon('#B7B4FF')} 
         />
         })}
         </>
