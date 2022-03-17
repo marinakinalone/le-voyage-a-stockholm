475cfd5767c3d08796195e88b95886d74e70f294
Salt Developer <salt-dev@Dev012.local>
2022-03-17 18:49:12 +0100

    implement useEffect on App

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,51 @@
 import './App.css';
-import { Header, Map } from './components/index';
+import { Header, Map, Details, ButtonSet } from './components/index';
 // import { Wrapper, Status } from "@googlemaps/react-wrapper";
 import { useEffect, useState } from 'react';
 
 const App = () => {
-  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true)
+  const [locationTypes, setLocationTypes] = useState([]);
+  const [locationData, setLocationData] = useState([]);
+
+
+  const fetchLocationData = async () => {
+    const locationInfoData = await fetch("/locations")
+    const locations = await locationInfoData.json();
+    setLocationData([...locations.locations])
+    const typesData = await fetch("/types")
+    const types = await typesData.json();
+    setLocationTypes([...types.types])
+    setTimeout(() => {
+      setLoading(false)
+    }, 1000)
+  }
 
   useEffect(() => {
-    fetch("/types")
-      .then(res => res.json())
-      .then(data => setData(data.types[0].type))
-  })
+    // fetch("/types")
+    //   .then(res => res.json())
+    //   .then(data => setLocationTypes([...data.types]))
+     
+  
+     fetchLocationData("/locations");
+  }, [])
 
   return (
     <div className="page">
-      <Header />
-      <Map />
-      <p>{data}</p>
+      {loading ? (
+        <p>loading...</p>
+      ) : (
+        <>
+        <Header />
+        <Map />
+        <section className="info-buttons">
+          <Details />
+          <ButtonSet />
+        </section>
+        <p>{locationData[0].name}</p>
+        <p>{locationTypes[0].type}</p>
+        </>
+      )}
     </div>
   )
 }
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,9 +1,15 @@
 import Header from './Header/Header';
 import Map from './Map/Map';
 import Markers from './Markers/Markers';
+import Details from './Details/Details';
+import ButtonSet from './ButtonSet/ButtonSet';
+import Button from './Button/Button';
 
 export {
     Header,
     Map,
-    Markers
+    Markers,
+    Details,
+    ButtonSet,
+    Button
 }
