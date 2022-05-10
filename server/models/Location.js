c79bf7601d390a164cd3fe02426790b67fd5d4b4
marinakinalone <kinalone.dev@gmail.com>
2022-05-10 18:15:25 +0200

    modify location model

--- server/models/Location.js
+++ server/models/Location.js
@@ -6,7 +6,7 @@ const locationSchema = new Schema({
   id: mongoose.SchemaTypes.ObjectId,
   name: String,
   position: Object,
-  type: String,
+  category: String,
   description: String,
   address: String,
   directions: String,
