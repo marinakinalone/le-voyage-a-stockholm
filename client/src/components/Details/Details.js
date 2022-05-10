ee34f11b30f8f4c7e2f1795897779b4aa9875145
marinakinalone <kinalone.dev@gmail.com>
2022-05-10 17:39:27 +0200

    update readme and fix fetched data in buttons set

--- client/src/components/Details/Details.js
+++ client/src/components/Details/Details.js
@@ -1,10 +1,8 @@
-import React from 'react'
-
-const Details = ({typesInfo}) => {
+const Details = ({ content }) => {
   return (
     <section className="details">
-        {typesInfo[0].name ? (<h3 className="details__title">{typesInfo[0].name}</h3>) : (<></>)}
-        <p className="details_description">{typesInfo[0].description}</p>
+        {content[0].name ? (<h3 className="details__title">{content[0].name}</h3>) : (<></>)}
+        <p className="details_description">{content[0].description}</p>
         <p className="details__links">créé par <a href="https://kinalone.dev">mks</a> | <a href="https://github.com/marinakinalone">GitHub</a></p>
     </section>
   )
