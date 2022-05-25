22427405c17146a3bdacb6562b7cee3a733ef66b
Marina Kinalone Simonnet <kinalone.dev@gmail.com>
2022-05-25 16:54:34 +0200

    modify marker

--- client/src/components/Markers/Markers.js
+++ client/src/components/Markers/Markers.js
@@ -2,13 +2,13 @@ import { Marker} from '@react-google-maps/api';
 import { markerIcon } from './helpers';
 
 const Markers = ({ locations, categories, getDescriptionByMarker }) => {
-    const handleClick = (id) =>{
+    const handleClick = (id, location) =>{
         getDescriptionByMarker(id)
     }
 
-    const getColor = (categoriesList, locationCategory) => {
+    const getColor = (categoriesList, locationCategory, state) => {
         const index = categoriesList.findIndex(item => item.category === locationCategory)
-        return categoriesList[index].colors.default;
+        return categoriesList[index].colors[state];
     }
 
     return (
@@ -17,9 +17,9 @@ const Markers = ({ locations, categories, getDescriptionByMarker }) => {
             return <Marker
             key={location._id}
             position={location.position}
-            icon={markerIcon(getColor(categories, location.category))}
+            icon={markerIcon(getColor(categories, location.category, "default"))}
             clickable={true}
-            onClick={() => handleClick(location._id)}
+            onClick={() => handleClick(location._id, location)}
         />
         })}
         </>
