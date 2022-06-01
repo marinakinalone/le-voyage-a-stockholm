93d6ae84132f018bc47306939aaa3a93a9174e50
Marina Kinalone Simonnet <kinalone.dev@gmail.com>
2022-06-01 16:34:51 +0200

    add category title to details component

--- client/src/components/ButtonSet/ButtonSet.js
+++ client/src/components/ButtonSet/ButtonSet.js
@@ -15,9 +15,9 @@ const ButtonSet = ({ categories, getLocationsByType, getDescriptionByType, setAc
       <section className="buttons">
           {categories.map(category => {
             if (activeCategory === category.category) {
-              return(<button className="buttons__btn" style={{backgroundColor: category.colors.default, boxShadow: "-3px -3px 0px 0px black", transform: "translate(2px, 2px)"}} onClick={() => handleChangeDisplay(category.category)}>{category.category}</button>)
+              return(<button key={category.category} className="buttons__btn" style={{backgroundColor: category.colors.default, boxShadow: "-3px -3px 0px 0px black", transform: "translate(2px, 2px)"}} onClick={() => handleChangeDisplay(category.category)}>{category.category}</button>)
             }
-            return(<button className="buttons__btn" style={{backgroundColor: category.colors.default}} onClick={() => handleChangeDisplay(category.category)}>{category.category}</button>)
+            return(<button key={category.category} className="buttons__btn" style={{backgroundColor: category.colors.default}} onClick={() => handleChangeDisplay(category.category)}>{category.category}</button>)
           })}
       </section>
   )
--- client/src/components/Details/Details.js
+++ client/src/components/Details/Details.js
@@ -1,8 +1,8 @@
 const Details = ({ content }) => {
   return (
     <section className="details">
-        {content[0].name ? (<h2 className="details__title">{content[0].name}</h2>) : (<></>)}
-        <p className="details_description">{content[0].description}</p>
+        {content[0].name ? (<h2 className="details__title">{content[0].name}</h2>) : (<h2 className="details__title">{content[0].category}</h2>)}
+        <p className="details__description">{content[0].description}</p>
         <p className="details__links">créé par <a href="https://kinalone.dev" target="_blank" rel="noopener noreferrer">mks</a> | <a href="https://github.com/marinakinalone" target="_blank" rel="noopener noreferrer">github</a></p>
     </section>
   )
